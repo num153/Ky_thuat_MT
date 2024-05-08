d2bf60e6d5a643db39a3bafde8804f64b3765a78
num153 <157980092+num153@users.noreply.github.com>
2024-05-08 13:59:25 +0700

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -2,10 +2,36 @@
 #include <iostream>
 #include <string>
 using namespace std;
+struct Date{
+    int date;
+    int month;
+    int year;
+    Date(){}
+    Date(int _d, int _m, int _y){
+        date = _d;
+        month=_m;
+        year=_y;
+    }
+};
 struct SV{
+    //properties
     string ten;
     int maso;
     float diem;
+    Date dob;
+    //constructor
+    SV(){}
+    SV(string _name, int _ma,float _diem,Date _dob){
+        ten = _name;
+        maso = _ma;
+        diem = _diem;
+        dob = _dob;
+    }
+    //method
+    void run(){
+        cout << "...running...\n";
+    }
+    
 };
 typedef struct SV svien;
 void nhap1sv(struct SV &sinhvien){
@@ -18,12 +44,13 @@ void xuat1sv(struct SV sinhvien){
     cout << "Ten: " << sinhvien.ten << endl;
     cout << "MSSV: " << sinhvien.maso << endl;
     cout << "Diem: " << sinhvien.diem << endl;
+    cout << "DOB: " << sinhvien.dob.date << "/" << sinhvien.dob.month << "/" << sinhvien.dob.year << endl;
 }
 int main() {
-    SV x;
-    cout << "Nhap thong tin SV x: " << endl;
-    nhap1sv(x);
+    SV x("Nam", 123,5.5, Date(15,3,2005));
+    //nhap1sv(x);
     xuat1sv(x);
+    x.run();
 
     return 0;
 }
