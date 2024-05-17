cfe5431fd53b3dcbf64300a3a19c1e9816be8ae3
num_153 <157980092+num153@users.noreply.github.com>
2024-05-17 18:55:41 +0700

    Create chuan_hoa_string.cpp

---
+++ chuan_hoa_string.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+void xoaDauCuoi(string &s){
+    //Xoa khoang trang dau & cuoi 
+    while(!s.empty() && s[0] == ' '){
+        s.erase(0,1);
+    }
+    while(!s.empty() && s[s.length()-1] == ' '){
+        s.erase(s.length()-1,1);
+    }
+}
+void xoa2Space(string &s){
+    //Moi tu chi cach nhau mot khoang trang
+    int i=0;
+    while(!s.empty() && i<s.length()){
+        if(s[i] == ' ' && s[i+1] == ' ')
+        {
+        s.erase(i,1);
+        }
+        else i++;
+    }
+}
+void inHoadau(string &s){
+    //chuyen chuoi thanh in thuong het
+    for(int i =0;i<s.length();i++){
+        s[i] = tolower(s[i]);
+    }
+    //viet hoa dau moi tu
+    for(int i=0;i<s.length();i++){
+        if(i == 0 || s[i-1] == ' ' && isalpha(s[i])){
+            s[i] = toupper(s[i]);
+        }
+    }
+}
+
+int main() {
+    string str;
+    cout << "Nhap chuoi: "; getline(cin, str);
+    xoaDauCuoi(str);
+    cout << "Chuoi sau khi xoa space dau va cuoi: \n" << str << endl;
+    xoa2Space(str);
+    cout << "Chuoi sau khi xoa cac khoang trang thua: \n" << str << endl;
+    inHoadau(str);
+    cout << "Chuoi sau khi viet hoa tu dau tien: \n" << str << endl;
+    return 0;
+}
