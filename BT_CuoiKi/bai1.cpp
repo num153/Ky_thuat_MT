854a854ca96b8d91e81c8b7cf8964e06b43c5cd4
num_153 <157980092+num153@users.noreply.github.com>
2024-05-19 23:31:33 +0700

    Update bai1.cpp

--- BT_CuoiKi/bai1.cpp
+++ BT_CuoiKi/bai1.cpp
@@ -54,7 +54,7 @@ void demCau(string &s){
             dem++;
         }
     }
-    cout << "So cau: " << dem;
+    cout << "So cau: " << dem << endl;
 }
 void demDaucau(string &s){
     int dem =0;
@@ -70,7 +70,8 @@ void demSotu(string &s){
             dem++;   
     }
     }
-    cout << "So tu: " << dem;
+    cout << "So tu: " << dem << endl;
+    // tuy nhien xin chao,con cai,ai << 3 la sai
 }
 void check(string s, bool func(string s)){
     if(func(s)){
@@ -80,11 +81,48 @@ void check(string s, bool func(string s)){
 }
 int main()
 {   
-    string str; char x;
+    string str; char x; int chon;
     cout << "Nhap chuoi: "; getline(cin, str);
     check(str,isNum);
     cout << str << endl;
-    demCau(str);
-    demSotu(str);
+    do{
+        cout << "\n============@_@============\n";
+        cout << "1. Kiem tra co so khong\n";
+        cout << "2. Kiem tra co space dau va cuoi khong\n";
+        cout << "3. Tach bo space\n";
+        cout << "4. Tach tung chu\n";
+        cout << "5. Dem so cau\n";
+        cout << "6.Dem so tu\n";
+        cout << "7. Dem so dau cau\n";
+        cout << "8. Tra ve ten dia phuong ung voi 3 ki tu dau\n";
+        cout << "0. Thoat\n";
+        cout << "Nhap lua chon: ";
+        cin >> chon;
+        switch(chon){
+            case 1:
+                check(str,isNum);
+                break;
+            case 2:
+                check(str,spaceDauCuoi);
+                break;
+            case 3: 
+                xoaSpaceDauCuoiGiua(str);
+                cout << str;
+                break;
+            case 4:
+                tachToken(str,x);
+                break;
+            case 5:
+                demCau(str);
+                break;
+            case 6:
+                demSotu(str);
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+        }
+    }while(chon!=0);
     return 0;
 }
